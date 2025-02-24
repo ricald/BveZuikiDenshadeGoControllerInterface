85063cf8022950149e4894f1a8084a284fedbf6d
Ricald <ricald21@gmail.com>
2025-02-25 03:26:30 +0900

    v1.0.2

--- make.bat
+++ make.bat
@@ -1,20 +1,27 @@
 @echo off
 
 set FRAMEWORK_PATH=%windir%\Microsoft.NET\Framework\v3.5\msbuild.exe
-set PLATFORM=Any CPU
 
 pushd source\Ricald.ZuikiMasconInterface
 
 echo Cleaning project...
-%FRAMEWORK_PATH% Ricald.ZuikiMasconInterface.csproj /p:Platform="%PLATFORM%" /target:clean /p:Configuration=Debug
+%FRAMEWORK_PATH% Ricald.ZuikiMasconInterface.csproj /p:Platform=x86 /target:clean /p:Configuration=Debug
 if errorlevel 1 goto error
-%FRAMEWORK_PATH% Ricald.ZuikiMasconInterface.csproj /p:Platform="%PLATFORM%" /target:clean /p:Configuration=Release
+%FRAMEWORK_PATH% Ricald.ZuikiMasconInterface.csproj /p:Platform=x86 /target:clean /p:Configuration=Release
+if errorlevel 1 goto error
+%FRAMEWORK_PATH% Ricald.ZuikiMasconInterface.csproj /p:Platform=x64 /target:clean /p:Configuration=Debug
+if errorlevel 1 goto error
+%FRAMEWORK_PATH% Ricald.ZuikiMasconInterface.csproj /p:Platform=x64 /target:clean /p:Configuration=Release
 if errorlevel 1 goto error
 
 echo Building project...
-%FRAMEWORK_PATH% Ricald.ZuikiMasconInterface.csproj /p:Platform="%PLATFORM%" /target:build /p:Configuration=Debug
+%FRAMEWORK_PATH% Ricald.ZuikiMasconInterface.csproj /p:Platform=x86 /target:build /p:Configuration=Debug
 if errorlevel 1 goto error
-%FRAMEWORK_PATH% Ricald.ZuikiMasconInterface.csproj /p:Platform="%PLATFORM%" /target:build /p:Configuration=Release
+%FRAMEWORK_PATH% Ricald.ZuikiMasconInterface.csproj /p:Platform=x86 /target:build /p:Configuration=Release
+if errorlevel 1 goto error
+%FRAMEWORK_PATH% Ricald.ZuikiMasconInterface.csproj /p:Platform=x64 /target:build /p:Configuration=Debug
+if errorlevel 1 goto error
+%FRAMEWORK_PATH% Ricald.ZuikiMasconInterface.csproj /p:Platform=x64 /target:build /p:Configuration=Release
 if errorlevel 1 goto error
 
 popd
